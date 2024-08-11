3ab4092ccce12d629352516acf7f6d5f2a45c6e9
Raghavendra baheti <bahetiraghav990@gmail.com>
2024-08-11 23:53:14 +0530

    day2

---
+++ code.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+public class code {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); 
+        int b = sc.nextInt();
+        int c = sc.nextInt(); 
+        int x = sc.nextInt(); 
+            
+           
+            if (a == x || b == x || c == x) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        
+    }
+
+
